69a37678ecbcd4802b3828e71f84a95994346840
samuelhargrove-code <samuel.hargrove@unitedgrowthsystem.com>
2026-04-07 14:56:02 -0400

    Create MapComponent.tsx

---
+++ frontend/components/MapComponent.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix leaflet default icon
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
+interface MapComponentProps {
+  onMapClick: (lat: number, lng: number) => void;
+  selectedCoords: [number, number] | null;
+}
+
+function ClickHandler({ onMapClick }: { onMapClick: (lat: number, lng: number) => void }) {
+  useMapEvents({
+    click(e) {
+      onMapClick(e.latlng.lat, e.latlng.lng);
+    }
+  });
+  return null;
+}
+
+export default function MapComponent({ onMapClick, selectedCoords }: MapComponentProps) {
+  return (
+    <MapContainer
+      center={[35.7796, -78.6382]}
+      zoom={10}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <ClickHandler onMapClick={onMapClick} />
+      {selectedCoords && (
+        <Marker position={selectedCoords} />
+      )}
+    </MapContainer>
+  );
+}
